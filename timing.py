b782dc2383c765d972b58a772a7558c7c2b250fa
amyhlback <arvid.myhlback@gmail.com>
2020-12-03 12:40:38 +0100

    Added newline at eof (because Github complained)

--- timing.py
+++ timing.py
@@ -2,9 +2,9 @@ import atexit
 from time import time
 
 def endlog():
-    end = time()
-    elapsed = end-start
-    print("\nElapsed time:", round(elapsed*1000, 3), "ms")    
+	end = time()
+	elapsed = end-start
+	print("\nElapsed time:", round(elapsed*1000, 3), "ms")
 
 start = time()
 atexit.register(endlog)
