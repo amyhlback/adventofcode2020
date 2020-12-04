797ef32448818679e670c259e47c542db6c1572e
amyhlback <arvid.myhlback@gmail.com>
2020-12-04 08:35:03 +0100

    Added laptime in timing

--- timing.py
+++ timing.py
@@ -1,10 +1,19 @@
 import atexit
 from time import time
 
+start = time()
+lap = start
+
 def endlog():
 	end = time()
 	elapsed = end-start
 	print("\nElapsed time:", round(elapsed*1000, 3), "ms")
 
-start = time()
+def laptime(text = ""):
+	end = time()
+	global lap
+	elapsed = end-lap
+	print("Laptime:", round(elapsed*1000, 3), "ms", text)
+	lap = time()
+
 atexit.register(endlog)
